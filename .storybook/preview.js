24f5a191f30f68aac0f40d7f6c1fae253a088ad2
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-01-06 19:49:50 -0600

    chore: configure storybook with theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import '../src/styles/globals.css';
+import { THERIFY_THEME } from '../src/styles/theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -9,3 +11,7 @@ export const parameters = {
         },
     },
 };
+
+export const decorators = [
+    (Story) => <ThemeProvider theme={THERIFY_THEME}>{Story()}</ThemeProvider>,
+];
