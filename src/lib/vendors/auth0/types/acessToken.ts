70043193d6f0bcda2edf43243d9624bd1ff62b40
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-01-07 23:10:36 -0600

    chore: prefer enum to string

--- src/lib/vendors/auth0/types/acessToken.ts
+++ src/lib/vendors/auth0/types/acessToken.ts
@@ -3,7 +3,7 @@ import * as z from 'zod';
 export const schema = z.object({
     access_token: z.string(),
     expires_in: z.number(),
-    token_type: z.string(),
+    token_type: z.enum(['Bearer']),
     scope: z.string(),
 });
 
