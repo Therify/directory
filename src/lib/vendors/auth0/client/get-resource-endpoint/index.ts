10d0aeb71ea7e302c0225db243845a75edda42fe
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-01-07 23:16:00 -0600

    chore: export configured implementation

--- src/lib/vendors/auth0/client/get-resource-endpoint/index.ts
+++ src/lib/vendors/auth0/client/get-resource-endpoint/index.ts
@@ -1,1 +1,7 @@
-export * from './getResourceEndpoint';
+import { withAuth0Configuration } from '../../configuration';
+import { getResourceEndpointFactory } from './getResourceEndpoint';
+
+export const getResourceEndpoint = withAuth0Configuration((CONFIG) => {
+    const getResourceEndpoint = getResourceEndpointFactory(CONFIG);
+    return getResourceEndpoint;
+});
