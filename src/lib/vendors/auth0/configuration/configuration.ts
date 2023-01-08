3bc0191572bec8136b3fdb0489a6ed736d3fdee9
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-01-07 22:13:56 -0600

    chore: auth0 configuration

---
+++ src/lib/vendors/auth0/configuration/configuration.ts
@@ -0,0 +1,50 @@
+import { get } from 'env-var';
+import { generateWithConfig } from '@/lib/utils';
+
+/**
+ * Auth0 Domain - The domain of your Auth0 tenant
+ * @see https://auth0.com/docs/get-started/dashboard
+ */
+const AUTH0_DOMAIN = 'AUTH0_DOMAIN' as const;
+/**
+ * Auth0 Client ID - The client ID of your Auth0 application
+ * @see https://auth0.com/docs/applications
+ * @see https://auth0.com/docs/applications/set-up-an-application
+ */
+const AUTH0_CLIENT_ID = 'AUTH0_CLIENT_ID' as const;
+/**
+ * Auth0 Client Secret - The client secret of your Auth0 application
+ * @see https://auth0.com/docs/applications
+ */
+const AUTH0_CLIENT_SECRET = 'AUTH0_CLIENT_SECRET' as const;
+
+/**
+ * Auth0 Configuration
+ * @property {string} AUTH0_DOMAIN - The domain of your Auth0 tenant
+ * @property {string} AUTH0_CLIENT_ID - The client ID of your Auth0 application
+ * @property {string} AUTH0_CLIENT_SECRET - The client secret of your Auth0 application
+ */
+interface Auth0Configuration {
+    AUTH0_DOMAIN: string;
+    AUTH0_CLIENT_ID: string;
+    AUTH0_CLIENT_SECRET: string;
+}
+
+/**
+ * Get the Auth0 configuration
+ * @param overrides - Override the default configuration
+ * @returns {Auth0Configuration}
+ */
+export const getAuth0Configuration = (
+    overrides: Partial<Auth0Configuration> = {}
+) => ({
+    [AUTH0_DOMAIN]: get(AUTH0_DOMAIN).required().asString(),
+    [AUTH0_CLIENT_ID]: get(AUTH0_CLIENT_ID).required().asString(),
+    [AUTH0_CLIENT_SECRET]: get(AUTH0_CLIENT_SECRET).required().asString(),
+    ...overrides,
+});
+
+/**
+ * Passes the Auth0 configuration to the provided function
+ */
+export const withAuth0Configuration = generateWithConfig(getAuth0Configuration);
