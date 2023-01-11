abf156557d5a9e43334e935b2815ae9b49a0936a
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-01-11 11:11:40 -0600

    chore: wrap app with App container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import { THERIFY_THEME } from '../styles/theme';
+import { ApplicationContainer } from '@/components/ui/Layout/Containers/ApplicationContainer';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={THERIFY_THEME}>
-            <Component {...pageProps} />
+            <ApplicationContainer>
+                <Component {...pageProps} />
+            </ApplicationContainer>
         </ThemeProvider>
     );
 }
