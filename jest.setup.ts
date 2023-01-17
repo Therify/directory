4bff8e3003bf1a28ce7d01f3fada37707faa77d3
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-01-17 12:25:38 -0600

    test: add update variable name to setup file

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 
 process.env = Object.assign(process.env, {
-    AUTH0_DOMAIN: 'test.auth0.com',
+    AUTH0_BACKEND_DOMAIN: 'test.auth0.com',
     AUTH0_BACKEND_CLIENT_ID: 'test-client-id',
     AUTH0_BACKEND_CLIENT_SECRET: 'dont-tell-nobody',
     STRIPE_SECRET_KEY: 'test-secret',
