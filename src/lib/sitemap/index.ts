c7f1057508f6f702dabfc7dee5caaabebb3b0a72
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-01-18 11:05:31 -0600

    chore: update site map

--- src/lib/sitemap/index.ts
+++ src/lib/sitemap/index.ts
@@ -1,7 +1,11 @@
+import { API_PATHS } from './api';
 import { AUTHENTICATION_PATHS } from './authentication';
 import { ONBOARDING_PATHS } from './onboarding';
+import { PROVIDERS_PATHS } from './providers';
 
 export const URL_PATHS = {
+    API: API_PATHS,
     AUTH: AUTHENTICATION_PATHS,
     ONBOARDING: ONBOARDING_PATHS,
+    PROVIDERS: PROVIDERS_PATHS,
 } as const;
---
+++ src/lib/sitemap/providers/index.ts
@@ -0,0 +1,4 @@
+export const PROVIDERS_PATHS = {
+    REGISTER: '/providers/register',
+    REGISTER_SUCCESS: '/providers/register/success',
+} as const;
