0c05376e00886e4af2a800e837220f8ff9352b36
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-01-20 13:12:37 -0600

    chore: move images to public/assets folder

--- src/components/ui/Shapes/AbstractShapes.tsx
+++ src/components/ui/Shapes/AbstractShapes.tsx
@@ -1,18 +1,26 @@
-import shape1 from './assets/abstract-shape-1.svg';
-import shape2 from './assets/abstract-shape-2.svg';
-import shape3 from './assets/abstract-shape-3.svg';
-
 type AbstractShapeProps = Omit<
     React.ComponentPropsWithoutRef<'img'>,
     'src' | 'alt'
 >;
 
 export const AbstractShape1 = (props: AbstractShapeProps) => (
-    <img src={shape1} alt="Abstract shape #1" {...props} />
+    <img
+        src="/assets/abstract-shape-1.svg"
+        alt="Abstract shape #1"
+        {...props}
+    />
 );
 export const AbstractShape2 = (props: AbstractShapeProps) => (
-    <img src={shape2} alt="Abstract shape #2" {...props} />
+    <img
+        src="/assets/abstract-shape-2.svg"
+        alt="Abstract shape #2"
+        {...props}
+    />
 );
 export const AbstractShape3 = (props: AbstractShapeProps) => (
-    <img src={shape3} alt="Abstract shape #3" {...props} />
+    <img
+        src="/assets/abstract-shape-3.svg"
+        alt="Abstract shape #3"
+        {...props}
+    />
 );
