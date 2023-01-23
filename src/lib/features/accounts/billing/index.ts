1c42fcdde183d9101d30bfbb20b261645bce3bb3
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-01-22 23:06:21 -0600

    chore: add cancel and renew feature schema

--- src/lib/features/accounts/billing/index.ts
+++ src/lib/features/accounts/billing/index.ts
@@ -1,2 +1,4 @@
 export * as HandleGroupPracticePlanPayment from './handle-group-practice-plan-payment';
 export * as HandlePlanChange from './handle-plan-change';
+export * as RenewPlan from './renew-plan';
+export * as CancelPlan from './cancel-plan';
