f6f5541f4c393296cb24d3ca30759e819034cbb4
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-01-23 01:56:08 -0600

    chore: poll for plan status

--- src/pages/providers/onboarding/billing/success.tsx
+++ src/pages/providers/onboarding/billing/success.tsx
@@ -1,27 +1,125 @@
+import { useEffect, useRef, useState } from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { H1, CenteredContainer } from '@/components/ui';
+import {
+    FormContainer,
+    StepperContainer,
+    LoadingContainer,
+    H1,
+    Caption,
+    Paragraph,
+} from '@/components/ui';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { useEffect } from 'react';
 import { usePracticeOnboardingStorage } from '@/components/features/onboarding';
+import { trpc } from '@/lib/utils/trpc';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { PlanStatus } from '@prisma/client';
+import { useRouter } from 'next/router';
+import { URL_PATHS } from '@/lib/sitemap';
 
 export const getServerSideProps = withPageAuthRequired();
 
+const REGISTRATION_STEPS = ['Registration', 'Payment', 'Onboarding'] as const;
+const THIRTY_SECONDS = 1000 * 30;
+
 export default function BillingSuccessPage() {
+    const { isLoading: isLoadingUser, user } = useUser();
+    const router = useRouter();
     const { clearStorage } = usePracticeOnboardingStorage();
-
+    const [allowRefetch, setAllowRefetch] = useState(true);
     useEffect(clearStorage, [clearStorage]);
+    console.log('render');
+    const timeoutRef = useRef<number>();
 
+    useEffect(() => {
+        timeoutRef.current = window.setTimeout(() => {
+            setAllowRefetch(false);
+        }, THIRTY_SECONDS);
+        return () => {
+            window.clearTimeout(timeoutRef.current);
+        };
+    }, []);
+
+    const {
+        data: planStatus,
+        error: queryError,
+        isLoading: isLoadingPlan,
+        isRefetching,
+    } = trpc.useQuery(
+        [
+            'accounts.users.get-plan-status-by-user-id',
+            {
+                auth0Id: user?.sub ?? '',
+            },
+        ],
+        {
+            refetchInterval: allowRefetch
+                ? (data) => {
+                      console.log({ data });
+                      if (data && data.status !== null) {
+                          console.log('received status...');
+                          window.clearTimeout(timeoutRef.current);
+                          return false;
+                      }
+                      return 2000;
+                  }
+                : false,
+            enabled: Boolean(user?.sub),
+        }
+    );
+
+    useEffect(() => {
+        if (planStatus?.status === PlanStatus.active) {
+            router.push(URL_PATHS.PROVIDERS.ONBOARDING.INVITATIONS);
+        }
+    }, [planStatus?.status, router]);
+
+    const [planError] = planStatus?.errors ?? [];
+    const isPlanInctive =
+        typeof planStatus?.status === 'string' &&
+        planStatus.status !== PlanStatus.active;
+    const planStatusMessage = isPlanInctive
+        ? `Your plan is not active. Please contact support.`
+        : undefined;
+    const errorMessage = planError ?? queryError?.message ?? planStatusMessage;
+    const isLoading =
+        isLoadingUser ||
+        isLoadingPlan ||
+        isRefetching ||
+        planStatus?.status === null;
     return (
         <PageContainer>
-            <CenteredContainer fillSpace>
-                <H1>TODO: Billing Success Page</H1>
-                <ul>
-                    <li>Poll for new plans (await the webhook)</li>
-                    <li>Redirect to the practice dashboard</li>
-                    <li>Or Timeout and handle webhook stalling/failures</li>
-                </ul>
-            </CenteredContainer>
+            <InnerContent>
+                <StepperContainer
+                    currentStepIndex={1}
+                    steps={REGISTRATION_STEPS as unknown as string[]}
+                >
+                    <FormContainer errorMessage={errorMessage}>
+                        <LoadingContainer
+                            isLoading={isLoading}
+                            loadingTopSlot={
+                                <Header>Setting up your practice...</Header>
+                            }
+                            loadingBottomSlot={
+                                <Disclaimer>
+                                    This may take up to 60 seconds
+                                </Disclaimer>
+                            }
+                        >
+                            {errorMessage ? (
+                                <Header>There seems to be an issue...</Header>
+                            ) : (
+                                <Header>Setup complete!</Header>
+                            )}
+                            {isPlanInctive && (
+                                <Paragraph>
+                                    Your plan&apos;s status: {planStatus.status}
+                                </Paragraph>
+                            )}
+                        </LoadingContainer>
+                    </FormContainer>
+                </StepperContainer>
+            </InnerContent>
         </PageContainer>
     );
 }
@@ -31,4 +129,20 @@ const PageContainer = styled(Box)(({ theme }) => ({
     overflowY: 'auto',
     height: '100%',
     width: '100%',
+}));
+
+const InnerContent = styled(Box)(({ theme }) => ({
+    maxWidth: '1200px',
+    width: '100%',
+    padding: theme.spacing(12, 4.5),
+    margin: '0 auto',
+}));
+
+const Header = styled(H1)(({ theme }) => ({
+    ...theme.typography.h3,
+}));
+
+const Disclaimer = styled(Caption)(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    marginTop: theme.spacing(6),
 }));
