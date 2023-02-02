9e15eaa0f0b56f7fb197efbe6b3c5e5e149a562b
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-02-01 18:04:16 -0600

    chore: add feature schema

---
+++ src/lib/features/members/get-self-assessments-by-user-id/output.ts
@@ -0,0 +1,22 @@
+import { SelfAssessmentSchema } from '@/lib/schema';
+import * as z from 'zod';
+
+export const schema = z.object({
+    selfAssessments: z.array(SelfAssessmentSchema),
+    errors: z.array(z.string()),
+});
+
+export type Output = z.infer<typeof schema>;
+
+export const validate = (value: unknown): Output => {
+    return schema.parse(value);
+};
+
+export const isValid = (value: unknown): value is Output => {
+    try {
+        validate(value);
+        return true;
+    } catch {
+        return false;
+    }
+};
---
+++ src/lib/features/members/get-self-assessments-by-user-id/trpc.ts
@@ -0,0 +1,1 @@
+export const ROUTE = 'users.get-self-assessments-by-user-id' as const;
