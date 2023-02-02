3ca99165a6288552869ab71053687433efb42bad
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-02-02 15:28:24 -0600

    chore: supress Next/Image warnings

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,8 +1,10 @@
 type LogoProps = Omit<React.ComponentPropsWithoutRef<'img'>, 'src' | 'alt'>;
 
 export const TherifyLogo = (props: LogoProps) => (
+    // eslint-disable-next-line @next/next/no-img-element
     <img src="/assets/therify-logo.svg" alt="Therify, Inc logo" {...props} />
 );
 export const TherifyIcon = (props: LogoProps) => (
+    // eslint-disable-next-line @next/next/no-img-element
     <img src="/assets/therify-icon.svg" alt="Therify, Inc icon" {...props} />
 );
