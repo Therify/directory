9425271e335fdbef2a7e59664b4e7077036e2ba4
Jessie Wooten <jessie@therify.co>
2023-02-16 03:52:32 -0600

    chore: remove auth0 email verification (#106)
    * chore: remove auth0 email verification
    
    * chore: set verify_email false on auth0.createUser

--- src/lib/modules/accounts/service/service/register-member/transaction/createAuth0User.ts
+++ src/lib/modules/accounts/service/service/register-member/transaction/createAuth0User.ts
@@ -1,11 +1,13 @@
-import { withNodeEnvironmentConfiguration } from '@/lib/shared/configuration';
+// import { withNodeEnvironmentConfiguration } from '@/lib/shared/configuration';
 import { TRANSACTION_STEPS, RegisterMemberTransaction } from './definition';
 
 export const createAuth0User: RegisterMemberTransaction['CREATE_AUTH0_USER'] = {
     async commit(input, { auth0 }) {
-        const shouldVerifyEmail = withNodeEnvironmentConfiguration(
-            (CONFIG) => CONFIG.NODE_ENV === 'production'
-        );
+        // TODO: Turn back on when Auth0 email is designed
+        // const shouldVerifyEmail = withNodeEnvironmentConfiguration(
+        //     (CONFIG) => CONFIG.NODE_ENV === 'production'
+        // );
+        const shouldVerifyEmail = false;
         const user = await auth0.createUser({
             email: input.emailAddress,
             password: input.password,
@@ -17,7 +19,7 @@ export const createAuth0User: RegisterMemberTransaction['CREATE_AUTH0_USER'] = {
         };
     },
     async rollback(
-        input,
+        _,
         { auth0 },
         { [TRANSACTION_STEPS.CREATE_AUTH0_USER]: { auth0UserId } }
     ) {
--- src/lib/modules/accounts/service/service/register-provider/transaction/createAuth0User.ts
+++ src/lib/modules/accounts/service/service/register-provider/transaction/createAuth0User.ts
@@ -11,11 +11,15 @@ interface CreateAuth0UserFactory {
 export const factory: CreateAuth0UserFactory = ({ emailAddress, password }) => {
     return {
         async commit({ auth0 }) {
+            // TODO: Turn back on when Auth0 email is designed
+            // const shouldVerifyEmail = withNodeEnvironmentConfiguration(
+            //     (CONFIG) => CONFIG.NODE_ENV === 'production'
+            // );
+            const shouldVerifyEmail = false;
             const { user_id: auth0UserId } = await auth0.createUser({
                 email: emailAddress,
                 password,
-                verify_email:
-                    getNodeEnvironmentConfiguration().NODE_ENV === 'production',
+                verify_email: shouldVerifyEmail,
             });
             if (!auth0UserId) throw new Error('No Auth0 user ID was returned.');
             return {
