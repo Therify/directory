301e82ef5b0f842876d8bacd95182149a356d0be
Jessie Wooten <jessie@therify.co>
2023-02-28 17:37:21 -0600

    fix: update unexpected stripe subscription types (#127)

--- src/lib/shared/vendors/stripe/types/discount.ts
+++ src/lib/shared/vendors/stripe/types/discount.ts
@@ -3,21 +3,23 @@ import * as z from 'zod';
 export const schema = z.object({
     customer: z.string(),
     discount: z.object({
-        coupon: z.object({
-            amount_off: z.number(),
-            created: z.number(),
-            currency: z.string(),
-            duration: z.string(),
-            duration_in_months: z.number(),
-            livemode: z.boolean(),
-            max_redemptions: z.number(),
-            metadata: z.object({}),
-            name: z.string(),
-            percent_off: z.number(),
-            redeem_by: z.number(),
-            times_redeemed: z.number(),
-            valid: z.boolean(),
-        }),
+        coupon: z
+            .object({
+                amount_off: z.number(),
+                created: z.number(),
+                currency: z.string(),
+                duration: z.string(),
+                duration_in_months: z.number(),
+                livemode: z.boolean(),
+                max_redemptions: z.number(),
+                metadata: z.object({}),
+                name: z.string(),
+                percent_off: z.number(),
+                redeem_by: z.number(),
+                times_redeemed: z.number(),
+                valid: z.boolean(),
+            })
+            .optional(),
         customer: z.string(),
         end: z.number(),
         id: z.string(),
