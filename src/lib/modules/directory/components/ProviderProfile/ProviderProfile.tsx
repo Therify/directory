21a00c5a93e66b41364d9c4cc4e0c315581596ca
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-03-03 15:43:45 -0600

    fix: missing availability widget on small screens

--- src/lib/modules/directory/components/ProviderProfile/ProviderProfile.tsx
+++ src/lib/modules/directory/components/ProviderProfile/ProviderProfile.tsx
@@ -569,7 +569,6 @@ export function ProviderProfile({
                                     md: 4,
                                 },
                                 display: {
-                                    sm: 'none',
                                     md: 'block',
                                 },
                                 paddingTop: {
