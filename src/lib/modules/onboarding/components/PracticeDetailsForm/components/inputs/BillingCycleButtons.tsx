6d4ff99d2ba607ddb714cb397441dd2993d66a00
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-03-13 15:24:35 -0500

    chore: copy change

--- src/lib/modules/onboarding/components/PracticeDetailsForm/components/inputs/BillingCycleButtons.tsx
+++ src/lib/modules/onboarding/components/PracticeDetailsForm/components/inputs/BillingCycleButtons.tsx
@@ -44,7 +44,7 @@ export const BillingCycleButtons = ({
                         Monthly
                     </ToggleButton>
                     <ToggleButton value={BILLING_CYCLE_OPTIONS.ANNUAL}>
-                        Annual
+                        Annually
                     </ToggleButton>
                 </ButtonGroup>
             )}
