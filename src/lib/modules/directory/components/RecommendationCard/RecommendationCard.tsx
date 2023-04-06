f7aa6d0d36d35224e78641b14c5cef0e2f312f80
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-04-06 15:52:59 -0500

    chore: navigate to provider page on click

--- src/lib/modules/directory/components/RecommendationCard/RecommendationCard.tsx
+++ src/lib/modules/directory/components/RecommendationCard/RecommendationCard.tsx
@@ -2,6 +2,7 @@ import {
     Avatar,
     AVATAR_SIZE,
     Button,
+    BUTTON_TYPE,
     Card,
     Paragraph,
     PARAGRAPH_SIZE,
@@ -11,52 +12,108 @@ import {
     VideoCameraFrontOutlined,
 } from '@mui/icons-material';
 import Chip from '@mui/material/Chip';
-import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import * as ProviderProfile from '@/lib/shared/types/provider-profile/providerProfile';
+import { z } from 'zod';
+import router from 'next/router';
+import { renderDesignation } from '@/lib/shared/types/provider-profile/render-designation/renderDesignation';
 
-export function RecommendationCard() {
+const recommendationCardSchema = ProviderProfile.schema.pick({
+    id: true,
+    profileImageUrl: true,
+    givenName: true,
+    surname: true,
+    designation: true,
+    offersVirtual: true,
+    offersInPerson: true,
+    specialties: true,
+});
+
+type RecommendationCardProps = {
+    recommendation: z.infer<typeof recommendationCardSchema>;
+};
+
+export function RecommendationCard({
+    recommendation,
+}: RecommendationCardProps) {
     return (
         <Card
             sx={{ bgcolor: 'white' }}
+            withBoxShadow
             headerSlot={
                 <Stack spacing={4}>
-                    <Avatar size={AVATAR_SIZE.XLARGE} />
+                    <Avatar
+                        size={AVATAR_SIZE.XLARGE}
+                        src={recommendation.profileImageUrl || undefined}
+                    />
                     <Stack spacing={0}>
                         <Paragraph
                             mb={0}
                             sx={{ fontSize: '22px', fontWeight: 'bold' }}
                         >
-                            Denise Richards
+                            {recommendation.givenName} {recommendation.surname}
                         </Paragraph>
                         <Paragraph size={PARAGRAPH_SIZE.SMALL}>
-                            Mental Health Coach
+                            {renderDesignation(recommendation)}
                         </Paragraph>
                     </Stack>
                     <Stack direction={'row'}>
-                        <SessionTypeChip
-                            label="Virtual"
-                            size="small"
-                            icon={<VideoCameraFrontOutlined />}
-                            variant="outlined"
-                            color="primary"
-                        />
+                        {recommendation.offersVirtual && (
+                            <SessionTypeChip
+                                label="Virtual"
+                                size="small"
+                                icon={<VideoCameraFrontOutlined />}
+                                variant="outlined"
+                                color="primary"
+                            />
+                        )}
+                        {recommendation.offersInPerson && (
+                            <SessionTypeChip
+                                label="In-Person"
+                                size="small"
+                                icon={<VideoCameraFront />}
+                                variant="outlined"
+                                color="primary"
+                            />
+                        )}
                     </Stack>
                 </Stack>
             }
             preBodySlot={<hr />}
             postBodySlot={
                 <Stack>
-                    <Paragraph size={PARAGRAPH_SIZE.SMALL}>
+                    <Paragraph
+                        size={PARAGRAPH_SIZE.SMALL}
+                        sx={{
+                            display: '-webkit-box',
+                            WebkitLineClamp: 3,
+                            WebkitBoxOrient: 'vertical',
+                            overflow: 'hidden',
+                        }}
+                    >
                         <span style={{ fontWeight: 'bold' }}>
-                            Specializes in:
+                            Specializes in:{' '}
                         </span>
+                        {recommendation.specialties.join(', ')}
                         <br />
                     </Paragraph>
-                    <Link>Select Provider</Link>
+                    <Button
+                        type={BUTTON_TYPE.TEXT}
+                        onClick={() => {
+                            router.push({
+                                pathname: `/members/directory/${recommendation.id}`,
+                                query: { selectProvider: true },
+                            });
+                        }}
+                    >
+                        Select Provider
+                    </Button>
                 </Stack>
             }
-            primaryButtonOnClick={console.log}
+            primaryButtonOnClick={() => {
+                router.push(`/members/directory/${recommendation.id}`);
+            }}
             fullWidthButton
             primaryButtonText={'View'}
         />
