017944b37e78e8224afcb018af10866cae214c3e
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-04-06 15:54:04 -0500

    chore: add progress to self-assessment submission

--- src/pages/members/care/self-assessment.tsx
+++ src/pages/members/care/self-assessment.tsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import { SelfAssessmentForm } from '@/lib/modules/members/components/SelfAssessmentForm';
 import { membersService } from '@/lib/modules/members/service';
 import { SelfAssessmentPageProps } from '@/lib/modules/members/service/get-self-assessment-page-props/getSelfAssessmentPageProps';
 import { MemberNavigationPage } from '@/lib/shared/components/features/pages';
+import { Modal } from '@/lib/shared/components/ui/Modal';
 import { PageHeader } from '@/lib/shared/components/ui/PageHeader';
 import { trpc } from '@/lib/shared/utils/trpc';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useRouter } from 'next/router';
+import { H2 } from '@/lib/shared/components/ui';
 
 export const getServerSideProps = membersService.getSelfAssessmentPageProps;
 
 export default function SelfAssessmentPage({ user }: SelfAssessmentPageProps) {
     const mutation = trpc.useMutation('members.create-self-assessment');
     const router = useRouter();
+    const [isModalOpen, setIsModalOpen] = useState(false);
     return (
         <MemberNavigationPage
             user={user}
@@ -26,6 +31,7 @@ export default function SelfAssessmentPage({ user }: SelfAssessmentPageProps) {
                 />
                 <SelfAssessmentForm
                     onSubmit={(formSubmission) => {
+                        setIsModalOpen(true);
                         mutation.mutate(
                             {
                                 userId: user.userId,
@@ -38,6 +44,30 @@ export default function SelfAssessmentPage({ user }: SelfAssessmentPageProps) {
                             }
                         );
                     }}
+                />
+                <Modal
+                    headerSlot={
+                        <>
+                            <H2 sx={{ textAlign: 'center' }}>
+                                Processing your assessment
+                            </H2>
+                        </>
+                    }
+                    postBodySlot={
+                        <Box
+                            sx={{
+                                width: '100%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                height: '100%',
+                            }}
+                        >
+                            <CircularProgress size={80} />
+                        </Box>
+                    }
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
                 />
             </PageContainer>
         </MemberNavigationPage>
