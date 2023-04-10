ebe74c1d51db54193dfede2fb5104744ec55a100
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-04-10 12:08:10 -0500

    chore: update user when deleting connect account

--- src/pages/api/dev/remove-connected-stripe-account.ts
+++ src/pages/api/dev/remove-connected-stripe-account.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { get } from 'env-var';
 import { vendorStripe } from '@/lib/shared/vendors/stripe';
+import { prisma } from '@/lib/prisma';
 
 const API_KEY = get('REMOVE_STRIPE_CONNECT_ACCOUNT').asString();
 
@@ -18,12 +19,28 @@ export default async function handler(
     }
     const { stripeConnectId } = req.body;
     if (!stripeConnectId || typeof stripeConnectId !== 'string') {
+        console.log('Invalid stripe connect id', { stripeConnectId });
         return res.status(400).end();
     }
+    const user = await prisma.user.findFirstOrThrow({
+        where: {
+            stripeConnectAccountId: stripeConnectId,
+        },
+    });
     console.log('Deleting Stripe connect account...');
     const { deleted } = await vendorStripe.deleteConnectAccount(
         stripeConnectId
     );
+    if (deleted) {
+        await prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                stripeConnectAccountId: null,
+            },
+        });
+    }
 
     return res.status(200).json({
         deleted,
