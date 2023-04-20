8a4e4e365967a8c6bad1815753c24f536f8ff601
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-04-19 19:20:06 -0500

    Revert "chore: require jotform submission id be unique"
    This reverts commit 0d500ebf0021312329629fb4a2974a894691f164.

--- prisma/migrations/20230420001455_make_jotform_submission_id_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[jotform_submission_id]` on the table `redeemed_sessions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "redeemed_sessions_jotform_submission_id_key" ON "redeemed_sessions"("jotform_submission_id");
