910eee2f8ceab50618eb5c85661e1dcf16e87b3c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-04-24 12:59:58 -0500

    chore: restrict prisma client creation

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,1 +1,7 @@
-export { default as prisma } from './client';
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
