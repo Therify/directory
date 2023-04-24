99a74cbe71c35bd0b3a0ca520a20fb99ddaacce4
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-04-23 20:17:40 -0500

    chore: save practice id on redeemed session

--- src/lib/modules/accounts/service/billing/handle-reimbursement-submission/transaction/getProviderDetails.ts
+++ src/lib/modules/accounts/service/billing/handle-reimbursement-submission/transaction/getProviderDetails.ts
@@ -11,15 +11,24 @@ export const factory: GetTherifyUserDetailsFactory = ({
     providerProfileId,
 }) => ({
     async commit({ prisma }) {
-        const { id } = await prisma.providerProfile.findUniqueOrThrow({
-            where: { id: providerProfileId },
-            select: {
-                id: true,
-            },
-        });
-
+        const { id, practiceProfile } =
+            await prisma.providerProfile.findUniqueOrThrow({
+                where: { id: providerProfileId },
+                select: {
+                    id: true,
+                    practiceProfile: {
+                        select: {
+                            practiceId: true,
+                        },
+                    },
+                },
+            });
+        if (!practiceProfile?.practiceId) {
+            throw new Error('Provider is not associated with a practice');
+        }
         return {
             providerProfileId: id,
+            practiceId: practiceProfile.practiceId,
         };
     },
     async rollback() {
