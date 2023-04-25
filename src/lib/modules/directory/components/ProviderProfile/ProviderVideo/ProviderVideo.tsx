da8f0b294cb3cd4f0523839195ab98555003a2bf
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-04-25 08:46:36 -0500

    chore: create provider video component

---
+++ src/lib/modules/directory/components/ProviderProfile/ProviderVideo/ProviderVideo.tsx
@@ -0,0 +1,124 @@
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import { type ProviderProfile } from '@/lib/shared/types';
+import { H6, Modal, Paragraph } from '@/lib/shared/components/ui';
+import { ArrowRight } from '@mui/icons-material';
+import { VideoPlayer } from '@/lib/shared/components/ui/VideoPlayer';
+import React from 'react';
+
+type ProviderVideoProps = Pick<
+    ProviderProfile.ProviderProfile,
+    'profileImageUrl' | 'givenName'
+> & {
+    videoPlayerProps: React.ComponentProps<typeof VideoPlayer>;
+};
+
+export function ProviderVideo({
+    profileImageUrl,
+    givenName,
+    videoPlayerProps,
+}: ProviderVideoProps) {
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+    return (
+        <Box>
+            <VideoContainer
+                direction="row"
+                onClick={() => setIsModalOpen(true)}
+            >
+                <PreviewImage profileImageUrl={profileImageUrl}>
+                    <PlayButtonContainer>
+                        <StyledPlayButton />
+                    </PlayButtonContainer>
+                </PreviewImage>
+                <VideoDescription>
+                    <Stack>
+                        <VideoTitle>Get to know {givenName}</VideoTitle>
+                        <VideoSubtitle>
+                            A short video about {givenName}
+                        </VideoSubtitle>
+                    </Stack>
+                </VideoDescription>
+            </VideoContainer>
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title={`Get to know ${givenName}`}
+                style={{ width: '100%', maxWidth: 800, minHeight: 500 }}
+                postBodySlot={
+                    <Box sx={{ height: '100%', width: '100%' }}>
+                        <VideoPlayer {...videoPlayerProps} />
+                    </Box>
+                }
+            />
+        </Box>
+    );
+}
+
+const VideoContainer = styled(Stack)(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.spacing(1),
+    height: 125,
+    overflow: 'hidden',
+}));
+const PreviewImage = styled(Box)<Pick<ProviderVideoProps, 'profileImageUrl'>>(
+    ({ theme, profileImageUrl }) => ({
+        maxWidth: 175,
+        position: 'relative',
+        backgroundImage: `url(${profileImageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ['hover']: {
+            cursor: 'pointer',
+        },
+        ['::after']: {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: 'rgba(0,0,0,0.2)',
+            zIndex: 1,
+        },
+    })
+);
+const PlayButtonContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    width: 50,
+    height: 50,
+    background: '#fff',
+    zIndex: 2,
+    ['hover']: {
+        cursor: 'pointer',
+        transform: 'scale(1.1)',
+    },
+}));
+const StyledPlayButton = styled(ArrowRight)(({ theme }) => ({
+    color: theme.palette.primary.main,
+    fontSize: 50,
+}));
+const VideoDescription = styled(Box)(({ theme }) => ({
+    flex: 1,
+    background: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    padding: theme.spacing(4),
+    display: 'flex',
+    alignItems: 'center',
+}));
+const VideoTitle = styled(H6)(({ theme }) => ({
+    ...theme.typography.body1,
+    fontSize: theme.typography.pxToRem(18),
+    fontWeight: 700,
+}));
+const VideoSubtitle = styled(Paragraph)(({ theme }) => ({
+    ...theme.typography.body2,
+    fontSize: theme.typography.pxToRem(16),
+}));
