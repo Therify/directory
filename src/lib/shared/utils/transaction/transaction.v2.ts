383eff2c9a719301f2d5740e727adc4573520b7d
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-06-13 14:40:30 -0500

    fix: throw error after transaction rollback

--- src/lib/shared/utils/transaction/transaction.v2.ts
+++ src/lib/shared/utils/transaction/transaction.v2.ts
@@ -102,6 +102,7 @@ export function generateTransaction<
                         );
                     }
                 }
+                throw error;
             }
         }
         return results;
