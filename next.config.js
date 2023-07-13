d323bd5453afae265f606ab02e93e6038c5fc41c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-07-13 12:28:12 -0500

    fix: transpile nylas deps
    @JessieWooten https://nextjs.org/docs/architecture/nextjs-compiler#module-transpilation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    transpilePackages: ['@nylas/nylas-react', '@nylas/nylas-js'],
     images: {
         domains: ['res.cloudinary.com'],
     },
