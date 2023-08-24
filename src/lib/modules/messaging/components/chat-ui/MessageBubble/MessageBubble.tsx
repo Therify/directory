2f754d28e80c798b5ffc9483d6281af3e914f285
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-08-24 12:58:50 -0500

    chore: update chat bubble style

--- src/lib/modules/messaging/components/chat-ui/MessageBubble/MessageBubble.tsx
+++ src/lib/modules/messaging/components/chat-ui/MessageBubble/MessageBubble.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { colors } from '@/lib/shared/components/themes/therify-design-system';
 
 interface MessageProps {
     children: ReactNode;
@@ -15,17 +16,13 @@ const Bubble = styled(Box, {
     shouldForwardProp: (prop) => prop !== 'isMine',
 })<{ isMine?: boolean }>(({ theme, isMine }) => ({
     display: 'inline-block',
-    backgroundColor: isMine
-        ? theme.palette.secondary.main
-        : theme.palette.grey[100],
+    backgroundColor: isMine ? colors.primary[50] : theme.palette.grey[100],
     padding: theme.spacing(4, 5),
     borderRadius: theme.spacing(3),
     ...(isMine
         ? { borderBottomRightRadius: 0 }
         : { borderBottomLeftRadius: 0 }),
-    color: isMine
-        ? theme.palette.secondary.contrastText
-        : theme.palette.text.primary,
+    color: theme.palette.text.primary,
     textAlign: isMine ? 'right' : 'left',
     width: 'auto',
     maxWidth: '100%',
