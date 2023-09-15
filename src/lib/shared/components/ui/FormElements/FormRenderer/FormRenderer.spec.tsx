e45f89e1443043f2a5f4d139c8317b804c39fe5c
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-09-15 17:19:48 -0500

    test: add telephone filed tests

--- src/lib/shared/components/ui/FormElements/FormRenderer/FormRenderer.spec.tsx
+++ src/lib/shared/components/ui/FormElements/FormRenderer/FormRenderer.spec.tsx
@@ -593,7 +593,6 @@ describe('FormRenderer', () => {
                     isSubmitting
                 />
             );
-            const password = '12345678';
             const input = getByPlaceholderText(inputDetails.placeholder!);
             expect(input).toBeDisabled();
         });
@@ -766,6 +765,104 @@ describe('FormRenderer', () => {
             ).firstElementChild;
 
             expect(select?.getAttribute('aria-disabled')).toBe('true');
+        });
+    });
+
+    describe('telephone field', () => {
+        const errorMessage = 'Phone number must be 10 characters.';
+        const mockSchema = z.object({
+            phone: z.string().nonempty().min(10, {
+                message: errorMessage,
+            }),
+        });
+        const mockConfig = getMockInputConfig({
+            id: 'test',
+            type: 'telephone',
+            label: 'Phone Number',
+            placeholder: '123',
+            helperText: 'Enter your number',
+            statePath: 'phone',
+            format: 'US',
+        });
+        const inputDetails = mockConfig.sections[0].fields[0] as PasswordInput<
+            z.infer<typeof mockSchema>
+        >;
+        it('renders telephone input', () => {
+            const { getByText } = renderWithTheme(
+                <FormRenderer
+                    validationSchema={mockSchema}
+                    config={mockConfig}
+                    validationMode={'onChange'}
+                    onSubmit={console.log}
+                />
+            );
+            expect(getByText(inputDetails.label)).toBeVisible();
+            expect(getByText(inputDetails.helperText!)).toBeVisible();
+        });
+
+        it('captures telephone input', async () => {
+            const mockSubmit = jest.fn();
+            const { getByPlaceholderText, getByText } = renderWithTheme(
+                <FormRenderer
+                    validationSchema={mockSchema}
+                    config={mockConfig}
+                    validationMode={'onChange'}
+                    onSubmit={mockSubmit}
+                />
+            );
+            const number = '1234567890';
+            const input = getByPlaceholderText(inputDetails.placeholder!);
+            const submitButton = getByText('Submit');
+            await user.type(input, number);
+            await user.click(submitButton);
+            expect(mockSubmit).toHaveBeenCalledWith({ phone: number });
+        });
+
+        it('masks telephone input', async () => {
+            const mockSubmit = jest.fn();
+            const { getByPlaceholderText } = renderWithTheme(
+                <FormRenderer
+                    validationSchema={mockSchema}
+                    config={mockConfig}
+                    validationMode={'onChange'}
+                    onSubmit={mockSubmit}
+                />
+            );
+            const number = '1234567890';
+            const input = getByPlaceholderText(inputDetails.placeholder!);
+            await user.type(input, number);
+            expect(input).toHaveValue('(123) 456-7890');
+        });
+
+        it('validates telephone input', async () => {
+            const { getByPlaceholderText, getByText } = renderWithTheme(
+                <FormRenderer
+                    validationSchema={mockSchema}
+                    config={mockConfig}
+                    validationMode={'onChange'}
+                    onSubmit={jest.fn()}
+                />
+            );
+            const userInput = '123';
+            const input = getByPlaceholderText(inputDetails.placeholder!);
+            const submitButton = getByText('Submit');
+            await user.type(input, userInput);
+            expect(getByText(errorMessage)).toBeVisible();
+            expect(submitButton).toBeDisabled();
+        });
+
+        it('disables field when form is submitting', () => {
+            const { getByPlaceholderText } = renderWithTheme(
+                <FormRenderer
+                    validationSchema={mockSchema}
+                    config={mockConfig}
+                    validationMode={'onChange'}
+                    onSubmit={jest.fn()}
+                    isSubmitting
+                />
+            );
+            const input = getByPlaceholderText(inputDetails.placeholder!);
+            expect(input).toBeDisabled();
         });
     });
 });
