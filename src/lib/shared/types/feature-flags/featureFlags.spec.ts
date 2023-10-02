ae6e817c59133e99277c782d60ddd46c4f6cc913
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-10-02 15:30:15 -0500

    test: fix failing tests due to rename

--- src/lib/shared/types/feature-flags/featureFlags.spec.ts
+++ src/lib/shared/types/feature-flags/featureFlags.spec.ts
@@ -1,45 +1,50 @@
-import { validate, defaultFlags } from './featureFlags';
+import { validateClientFlags, defaultClientFlags } from './featureFlags';
 
 describe('FeatureFlags validation', () => {
     it('should return default flags when flags are undefined', () => {
-        expect(validate(undefined)).toEqual(defaultFlags);
+        expect(validateClientFlags(undefined)).toEqual(defaultClientFlags);
     });
 
     it('should return default flags when flags are null', () => {
-        expect(validate(null)).toEqual(defaultFlags);
+        expect(validateClientFlags(null)).toEqual(defaultClientFlags);
     });
 
     it('should return default flags when flags are not an object', () => {
-        expect(validate('not an object')).toEqual(defaultFlags);
+        expect(validateClientFlags('not an object')).toEqual(
+            defaultClientFlags
+        );
     });
 
     it('should return default flags when flags are an empty object', () => {
-        expect(validate({})).toEqual(defaultFlags);
+        expect(validateClientFlags({})).toEqual(defaultClientFlags);
     });
 
     it('should return flags when flags are valid', () => {
         const flags = {
-            ...defaultFlags,
+            ...defaultClientFlags,
             didFlagsLoad: true,
         };
-        expect(validate(flags)).toEqual(flags);
+        expect(validateClientFlags(flags)).toEqual(flags);
     });
 
     it('should return default flag value when individual key is invalid', () => {
         const flags = {
-            ...defaultFlags,
+            ...defaultClientFlags,
             didFlagsLoad: 'not a boolean',
         };
-        expect(validate(flags)).toEqual(defaultFlags);
+        expect(validateClientFlags(flags)).toEqual(defaultClientFlags);
     });
 
     it('should merge partially valid flags with default flags', () => {
         const flags = {
-            ...defaultFlags,
+            ...defaultClientFlags,
             didFlagsLoad: true,
         };
         expect(
-            validate({ ...flags, invalidFlag: "this shouldn't be here" })
+            validateClientFlags({
+                ...flags,
+                invalidFlag: "this shouldn't be here",
+            })
         ).toEqual(flags);
     });
 });
