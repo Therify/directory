25c9b4c763d2a7425a3f71de2d659471eb2719ad
Joey Fitzpatrick <joeyfitzpatrick@Joeys-MBP.attlocal.net>
2023-10-03 18:09:24 -0500

    chore: formatting jumbotron.tsx

--- src/lib/shared/components/ui/Jumbotron/Jumbotron.tsx
+++ src/lib/shared/components/ui/Jumbotron/Jumbotron.tsx
@@ -1,10 +1,10 @@
-import { Box } from "@mui/material";
+import { Box } from '@mui/material';
 import { SxProps, Theme } from '@mui/material/styles';
 import { Button } from '../Button';
 
 import { styled } from '@mui/material/styles';
 import { H1, Paragraph } from '../Typography/';
-import { CelebrationContainer } from "../Containers";
+import { CelebrationContainer } from '../Containers';
 
 export const TEST_IDS = {
     CONTAINER: 'container',
@@ -19,17 +19,36 @@ export interface JumbotronProps {
         onClick?: () => void;
     };
     useCelebrationStyling?: boolean;
-    sx?: SxProps<Theme>
+    sx?: SxProps<Theme>;
 }
 
-export function Jumbotron({ sx, headerText, subHeaderText, backgroundImageUrl, callToAction, useCelebrationStyling }: JumbotronProps) {
+export function Jumbotron({
+    sx,
+    headerText,
+    subHeaderText,
+    backgroundImageUrl,
+    callToAction,
+    useCelebrationStyling,
+}: JumbotronProps) {
     return (
-        <Container data-testid={TEST_IDS.CONTAINER} backgroundImageUrl={backgroundImageUrl} sx={sx}>
+        <Container
+            data-testid={TEST_IDS.CONTAINER}
+            backgroundImageUrl={backgroundImageUrl}
+            sx={sx}
+        >
             {useCelebrationStyling && <CelebrationBackground />}
             <ContentContainer>
                 <Header>{headerText}</Header>
                 {subHeaderText && <SubHeader>{subHeaderText}</SubHeader>}
-                {callToAction && <CallToActionButton aria-label="button" useCelebrationStyling={useCelebrationStyling} onClick={callToAction.onClick}>{callToAction.label}</CallToActionButton>}
+                {callToAction && (
+                    <CallToActionButton
+                        aria-label="button"
+                        useCelebrationStyling={useCelebrationStyling}
+                        onClick={callToAction.onClick}
+                    >
+                        {callToAction.label}
+                    </CallToActionButton>
+                )}
             </ContentContainer>
         </Container>
     );
@@ -46,13 +65,15 @@ const SubHeader = styled(Paragraph)(({ theme }) => ({
 const CallToActionButton = styled(Button, {
     shouldForwardProp: (prop) =>
         !['useCelebrationStyling'].includes(prop.toString()),
-})<{ useCelebrationStyling: boolean | undefined }>(({ theme, useCelebrationStyling }) => ({
-    ...(useCelebrationStyling && {
-        background: 'rgba(204, 204, 204, 0.0)',
-        border: `1px solid ${theme.palette.common.white}`,
-        borderRadius: theme.shape.borderRadius,
-    }),
-}));
+})<{ useCelebrationStyling: boolean | undefined }>(
+    ({ theme, useCelebrationStyling }) => ({
+        ...(useCelebrationStyling && {
+            background: 'rgba(204, 204, 204, 0.0)',
+            border: `1px solid ${theme.palette.common.white}`,
+            borderRadius: theme.shape.borderRadius,
+        }),
+    })
+);
 
 const ContentContainer = styled(Box)(({ theme }) => ({
     display: 'flex',
@@ -66,40 +87,42 @@ const ContentContainer = styled(Box)(({ theme }) => ({
 const Container = styled(Box, {
     shouldForwardProp: (prop) =>
         !['backgroundImageUrl'].includes(prop.toString()),
-})<{ backgroundImageUrl: string | undefined }>(({ theme, backgroundImageUrl }) => ({
-    height: 258,
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    textAlign: 'center',
-    color: theme.palette.common.white,
-    padding: theme.spacing(7.5, 5),
-    position: 'relative',
-    overflow: 'hidden',
-    borderRadius: theme.shape.borderRadius,
-    ...(backgroundImageUrl && {
-        backgroundImage: `url(${backgroundImageUrl})`
-    }),
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    '::after': {
-        opacity: 0.6,
-        zIndex: 1,
-        position: 'absolute',
-        content: '""',
-        top: 0,
-        left: 0,
+})<{ backgroundImageUrl: string | undefined }>(
+    ({ theme, backgroundImageUrl }) => ({
+        height: 258,
         width: '100%',
-        height: '100%',
-        background: theme.palette.common.black,
-    },
-    [theme.breakpoints.up('md')]: {
-        height: 326,
-    },
-}));
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        textAlign: 'center',
+        color: theme.palette.common.white,
+        padding: theme.spacing(7.5, 5),
+        position: 'relative',
+        overflow: 'hidden',
+        borderRadius: theme.shape.borderRadius,
+        ...(backgroundImageUrl && {
+            backgroundImage: `url(${backgroundImageUrl})`,
+        }),
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        '::after': {
+            opacity: 0.6,
+            zIndex: 1,
+            position: 'absolute',
+            content: '""',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: theme.palette.common.black,
+        },
+        [theme.breakpoints.up('md')]: {
+            height: 326,
+        },
+    })
+);
 
 const CelebrationBackground = styled(CelebrationContainer)(({ theme }) => ({
     zIndex: 2,
