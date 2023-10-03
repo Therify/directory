2e92e2d19f3da63d828b212b5bd078f49c08c41d
Joey Fitzpatrick <48607329+JoeyFitzpatrick@users.noreply.github.com>
2023-10-03 13:29:32 -0500

    Feat/add jumbotron component (#280)
    * chore: initial commit for jumbotron
    
    * feat: jumbotron component added
    
    * test: tests for jumbotron component
    
    * chore: formatting, make jumbotron props more specific
    
    * chore: fix some styling and an export
    
    * chore: fix a circular dependency
    
    * chore: another circular dependency fix
    
    ---------
    
    Co-authored-by: Joey Fitzpatrick <joeyfitzpatrick@Joeys-MBP.attlocal.net>

---
+++ src/lib/shared/components/ui/Jumbotron/Jumbotron.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryFn, } from '@storybook/react';
+import { Jumbotron as JumbotronUi, JumbotronProps } from './Jumbotron';
+
+const meta: Meta<typeof JumbotronUi> = {
+    title: 'UI/Jumbotron',
+    component: JumbotronUi,
+};
+
+export default meta;
+
+const defaultProps: JumbotronProps = {
+    headerText: 'Build your Care Team...',
+    backgroundImageUrl: 'https://res.cloudinary.com/dbrkfldqn/image/upload/v1673453029/app.therify.co/login/8_jvntyl.jpg',
+    callToAction: {
+        label: 'Get Started',
+        onClick: () => alert('Clicked!'),
+    }
+}
+
+const subheaderProps: JumbotronProps = {
+    headerText: 'Top text',
+    subHeaderText: 'Bottom text',
+    backgroundImageUrl: 'https://res.cloudinary.com/dbrkfldqn/image/upload/v1673453029/app.therify.co/login/10_dj9do6.jpg',
+    callToAction: {
+        label: 'Please click me',
+        onClick: () => alert('Thank you'),
+    }
+}
+
+const noButtonProps: JumbotronProps = {
+    headerText: 'Top text',
+    subHeaderText: 'Bottom text',
+    backgroundImageUrl: 'https://res.cloudinary.com/dbrkfldqn/image/upload/v1673453029/app.therify.co/login/10_dj9do6.jpg',
+}
+
+export const Default: StoryFn<typeof JumbotronUi> = () => (
+    <JumbotronUi {...defaultProps} />
+);
+export const WithSubheader: StoryFn<typeof JumbotronUi> = () => (
+    <JumbotronUi {...subheaderProps} />
+);
+export const NoButton: StoryFn<typeof JumbotronUi> = () => (
+    <JumbotronUi {...noButtonProps} />
+);
---
+++ src/lib/shared/components/ui/Jumbotron/Jumbotron.tsx
@@ -0,0 +1,89 @@
+import { Box } from "@mui/material";
+import { SxProps, Theme } from '@mui/material/styles';
+import { Button } from '../Button';
+
+import { styled } from '@mui/material/styles';
+import { H1, Paragraph } from '../Typography/';
+
+export const TEST_IDS = {
+    CONTAINER: 'container',
+} as const;
+
+export interface JumbotronProps {
+    headerText: string;
+    subHeaderText?: string;
+    backgroundImageUrl?: string;
+    callToAction?: {
+        label: string;
+        onClick?: () => void;
+    }
+    sx?: SxProps<Theme>
+}
+
+export function Jumbotron({ sx, headerText, subHeaderText, backgroundImageUrl, callToAction }: JumbotronProps) {
+    return (
+        <Container data-testid={TEST_IDS.CONTAINER} backgroundImageUrl={backgroundImageUrl} sx={sx}>
+            <ContentContainer>
+                <Header>{headerText}</Header>
+                {subHeaderText && <SubHeader>{subHeaderText}</SubHeader>}
+                {callToAction && <Button aria-label="button" onClick={callToAction.onClick}>{callToAction.label}</Button>}
+            </ContentContainer>
+        </Container>
+    );
+}
+
+const Header = styled(H1)(({ theme }) => ({
+    ...theme.typography.h4,
+}));
+
+const SubHeader = styled(Paragraph)(({ theme }) => ({
+    ...theme.typography.body1,
+}));
+
+const ContentContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(5),
+    position: 'relative',
+    zIndex: 2,
+}));
+
+const Container = styled(Box, {
+    shouldForwardProp: (prop) =>
+        !['backgroundImageUrl'].includes(prop.toString()),
+})<{ backgroundImageUrl: string | undefined }>(({ theme, backgroundImageUrl }) => ({
+    height: 258,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    color: theme.palette.common.white,
+    padding: theme.spacing(7.5, 5),
+    position: 'relative',
+    overflow: 'hidden',
+    borderRadius: theme.shape.borderRadius,
+    ...(backgroundImageUrl && {
+        backgroundImage: `url(${backgroundImageUrl})`
+    }),
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    '::after': {
+        opacity: 0.6,
+        zIndex: 1,
+        position: 'absolute',
+        content: '""',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: theme.palette.common.black,
+    },
+    [theme.breakpoints.up('md')]: {
+        height: 326,
+    },
+}));
+
