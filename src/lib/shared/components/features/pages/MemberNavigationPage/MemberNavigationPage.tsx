75878728126c38b9a1308717b33ac5d970523a29
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-10-05 18:46:51 -0500

    chore: add v3 layout page to member nav page

--- src/lib/shared/components/features/pages/MemberNavigationPage/MemberNavigationPage.tsx
+++ src/lib/shared/components/features/pages/MemberNavigationPage/MemberNavigationPage.tsx
@@ -7,10 +7,12 @@ import { useChatClient } from '@/lib/modules/messaging/hooks';
 import { URL_PATHS } from '@/lib/sitemap';
 import { TherifyUser } from '@/lib/shared/types';
 import { useRouter } from 'next/router';
+import { useFeatureFlags } from '@/lib/shared/hooks';
 import {
-    TopNavigationPage,
+    TopNavigationPage as TopNavigationPageV2,
     TopNavigationPageProps,
 } from '../TopNavigationPage';
+import { TopNavigationPage as TopNavigationPageV3 } from '../v3/TopNavigationPage';
 
 export interface MemberNavigationPageProps
     extends Omit<
@@ -29,24 +31,37 @@ export function MemberNavigationPage(props: MemberNavigationPageProps) {
     const mainMenu = getMemberMenu(props.user?.hasChatEnabled ?? false);
     const mobileMenu = getMemberMobileMenu(props.user?.hasChatEnabled ?? false);
     const { ChatProvider, unreadChatMessagesCount } = useChatClient(props.user);
+    const { flags } = useFeatureFlags(props.user ?? undefined);
 
+    const chatNotifications =
+        unreadChatMessagesCount > 0
+            ? {
+                  [URL_PATHS.MEMBERS.CHAT]: unreadChatMessagesCount,
+              }
+            : undefined;
     return (
         <ChatProvider>
-            <TopNavigationPage
-                {...props}
-                onNavigate={router.push}
-                primaryMenu={mainMenu}
-                secondaryMenu={[...MEMBER_SECONDARY_MENU]}
-                mobileMenu={mobileMenu}
-                isLoadingUser={false}
-                chatNotifications={
-                    unreadChatMessagesCount > 0
-                        ? {
-                              [URL_PATHS.MEMBERS.CHAT]: unreadChatMessagesCount,
-                          }
-                        : undefined
-                }
-            />
+            {flags.isV3DirectoryEnabled ? (
+                <TopNavigationPageV3
+                    {...props}
+                    onNavigate={router.push}
+                    primaryMenu={mainMenu}
+                    secondaryMenu={[...MEMBER_SECONDARY_MENU]}
+                    mobileMenu={mobileMenu}
+                    isLoadingUser={false}
+                    chatNotifications={chatNotifications}
+                />
+            ) : (
+                <TopNavigationPageV2
+                    {...props}
+                    onNavigate={router.push}
+                    primaryMenu={mainMenu}
+                    secondaryMenu={[...MEMBER_SECONDARY_MENU]}
+                    mobileMenu={mobileMenu}
+                    isLoadingUser={false}
+                    chatNotifications={chatNotifications}
+                />
+            )}
         </ChatProvider>
     );
 }
