0a4e483b387a481fe3f4228666e2d5473a635089
Joey Fitzpatrick <joeyfitzpatrick@Joeys-MBP.attlocal.net>
2023-10-09 12:22:01 -0500

    test: improve readability for care jumbotron

--- src/lib/modules/care/components/CareJumbotron/CareJumbotron.spec.tsx
+++ src/lib/modules/care/components/CareJumbotron/CareJumbotron.spec.tsx
@@ -1,5 +1,5 @@
-import { renderWithTheme } from '../../../../shared/components/fixtures/renderWithTheme';
-import { CareJumbotron, CareJumbotronProps } from './CareJumbotron';
+import { renderWithTheme } from '@/lib/shared/components/fixtures/renderWithTheme';
+import { CareJumbotron } from './CareJumbotron';
 
 const routerMock = { pathname: '/test', push: jest.fn() };
 jest.mock('next/router', () => {
@@ -9,82 +9,96 @@ jest.mock('next/router', () => {
 });
 
 describe('CareJumbotron', () => {
-    it('should render', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={false}
-            hasRecommendationsForReview={false}
-            hasCareTeam={false}
-        />
-        );
-        const header = getByText('Get matched with your first Provider!');
-        expect(header).toBeVisible();
+    describe('hasRecommendationsForReview', () => {
+        it('should render message when there are recommendations for review', () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    hasRecommendationsForReview={true}
+                    isAwaitingRecommendations={false}
+                    hasCareTeam={false}
+                />
+            );
+            const header = getByText("You've got Provider Recommendations!");
+            expect(header).toBeVisible();
+        });
+        it('should override all other messages', () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    hasRecommendationsForReview={true}
+                    isAwaitingRecommendations={true}
+                    hasCareTeam={true}
+                />
+            );
+            const header = getByText("You've got Provider Recommendations!");
+            expect(header).toBeVisible();
+        });
     });
-    it('should render \'has provider recommendations\' message when there are recommendations for review', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={false}
-            hasRecommendationsForReview={true}
-            hasCareTeam={false}
-        />
-        );
-        const header = getByText('You\'ve got Provider Recommendations!');
-        expect(header).toBeVisible();
+    describe('isAwaitingRecommendations', () => {
+        it('should render message when awaiting recommendations', () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    isAwaitingRecommendations={true}
+                    hasRecommendationsForReview={false}
+                    hasCareTeam={false}
+                />
+            );
+            const header = getByText(
+                "We're looking for providers to recommend!"
+            );
+            expect(header).toBeVisible();
+        });
+        it("should render '{name} is looking for providers' message when awaiting recommendations from a specific care navigator", () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    isAwaitingRecommendations={true}
+                    hasRecommendationsForReview={false}
+                    hasCareTeam={false}
+                    careNavigatorName="Dr. Test"
+                />
+            );
+            const header = getByText(
+                'Dr. Test is looking for providers to recommend!'
+            );
+            expect(header).toBeVisible();
+        });
+        it("should override 'build care team' message", () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    isAwaitingRecommendations={true}
+                    hasRecommendationsForReview={false}
+                    hasCareTeam={true}
+                />
+            );
+            const header = getByText(
+                "We're looking for providers to recommend!"
+            );
+            expect(header).toBeVisible();
+        });
     });
-    it('should render \'has provider recommendations\' message when there are recommendations for review, overriding other props', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={true}
-            hasRecommendationsForReview={true}
-            hasCareTeam={true}
-        />
-        );
-        const header = getByText('You\'ve got Provider Recommendations!');
-        expect(header).toBeVisible();
+    describe('hasCareTeam', () => {
+        it("should render message when 'hasCareTeam' prop is true", () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    isAwaitingRecommendations={false}
+                    hasRecommendationsForReview={false}
+                    hasCareTeam={true}
+                />
+            );
+            const header = getByText('Build your Care Team!');
+            expect(header).toBeVisible();
+        });
     });
-    it('should render \'build care team\' message when \'hasCareTeam\' prop is true', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={false}
-            hasRecommendationsForReview={false}
-            hasCareTeam={true}
-        />
-        );
-        const header = getByText('Build your Care Team!');
-        expect(header).toBeVisible();
-    });
-    it('should render \'build care team\' message when \'hasCareTeam\' prop is true, overriding \'awaiting recommendations\' prop', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={true}
-            hasRecommendationsForReview={false}
-            hasCareTeam={true}
-        />
-        );
-        const header = getByText('Build your Care Team!');
-        expect(header).toBeVisible();
-    });
-    it('should render \'looking for providers\' message when awaiting recommendations', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={true}
-            hasRecommendationsForReview={false}
-            hasCareTeam={false}
-        />
-        );
-        const header = getByText('We\'re looking for providers to recommend!');
-        expect(header).toBeVisible();
-    });
-    it('should render \'{name} is looking for providers\' message when awaiting recommendations from a specific care navigator', () => {
-        const { getByText } = renderWithTheme(
-        <CareJumbotron
-            isAwaitingRecommendations={true}
-            hasRecommendationsForReview={false}
-            hasCareTeam={false}
-            careNavigatorName='Dr. Test'
-        />
-        );
-        const header = getByText('Dr. Test is looking for providers to recommend!');
-        expect(header).toBeVisible();
+    describe('default message', () => {
+        it("should default to 'Get Matched' message", () => {
+            const { getByText } = renderWithTheme(
+                <CareJumbotron
+                    isAwaitingRecommendations={false}
+                    hasRecommendationsForReview={false}
+                    hasCareTeam={false}
+                />
+            );
+            const header = getByText('Get matched with your first Provider!');
+            expect(header).toBeVisible();
+        });
     });
 });
