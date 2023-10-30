2bcca9cd4b95cabf31d7cf315531995ca4ceee53
Jessie Wooten <jessiewootenvisual@gmail.com>
2023-10-30 12:36:04 -0500

    chore: remove unused form config

--- src/lib/modules/accounts/components/settings/SettingsPage/views/NotificationsView/form/index.ts
+++ src/lib/modules/accounts/components/settings/SettingsPage/views/NotificationsView/form/index.ts
@@ -1,9 +1,7 @@
 import { schema } from './schema';
-import { config } from './config';
 
 export const notificationsForm = {
     schema,
-    config,
 };
 
 export type { Type as NotificationsForm } from './schema';
