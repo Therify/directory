12767870f68afde72d0f5d1f0bb5dc3cb09b4759
jessiewooten <jessie@therify.co>
2025-09-04 11:35:01 -0500

    chore: add deprecated page and reroutes

--- next.config.js
+++ next.config.js
@@ -13,6 +13,63 @@ const nextConfig = {
             transform: '@mui/material/{{member}}',
         },
     },
+    redirects: async () => [
+        {
+            source: '/accounts/:path*',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/directory/:path*',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/members/:path*',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/providers/:path*',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/api/:path*',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/access-countdown',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/register',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/waiting-list',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/404',
+            destination: '/deprecated',
+            permanent: true,
+        },
+        {
+            source: '/500',
+            destination: '/deprecated',
+            permanent: true,
+        }, 
+    ],
 };
 
 module.exports = nextConfig;
